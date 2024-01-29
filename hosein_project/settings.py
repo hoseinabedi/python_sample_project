59c0e3a10c1c81c09a6d9b3e628779ddcf364edc
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-01-29 13:51:25 +0330

    Turn debug mode for website to off

--- hosein_project/settings.py
+++ hosein_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^k$!c#93!62wv%#%zl2j9dccan=5h^u%!f09brom)^4xdm^*gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
