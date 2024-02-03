395815ab309deac3a3ddc18ceff4f5b847e15973
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-02-03 10:55:59 +0330

    Url fixed & model added slug

--- members/models.py
+++ members/models.py
@@ -6,3 +6,7 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.CharField(max_length=15, null=True)
     joined_date = models.DateTimeField(null=True)
+    slug = models.SlugField(default="", null=False)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
